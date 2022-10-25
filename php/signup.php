9c0c6e957a4a6cb175e4db2d14029d200417982d
Monirul <monirul.work.1729@gmail.com>
2022-10-25 19:52:35 +0530

    Completed SignUp functionality

--- php/signup.php
+++ php/signup.php
@@ -2,32 +2,36 @@
   include "connection.php";
 
   //this is to create new table
-  $sql="CREATE TABLE IF NOT EXISTS student(username varchar(40),  passwd varchar(40));";
+  $sql="CREATE TABLE IF NOT EXISTS student(reg_no varchar(9) NOT NULL PRIMARY KEY, username varchar(50), passwd varchar(50), phone_no varchar(10), email varchar(50), gender varchar(1), branch varchar(50), present_year int, fee_paid boolean);";
   $result=mysqli_query($conn, $sql);
-
-  if(! $result){
-    echo "Failed to create table";
-  }
-  echo "Created table!!";
+  // if(! $result){
+  //   echo "Failed to create table";
+  // }
+  // echo "Created table!!";
 
 
   //this is to add data to table
+
   $usrnm = $_POST["fname"];
   $pswd = $_POST["passwd"];
   $regno = $_POST["regno"];
   $emailId = $_POST["emailId"];
-  
 
   $username = stripcslashes($usrnm);
   $password = stripcslashes($pswd);
+  $regno = stripcslashes($regno);
+
   $username = mysqli_real_escape_string($conn,$username);
   $password = mysqli_real_escape_string($conn,$password);
+  $regno = mysqli_real_escape_string($conn,$regno);
+  $emailId = mysqli_real_escape_string($conn,$emailId);
 
-  $sql = "INSERT INTO studentTemp VALUES('$username', '$password');";
-  $result = mysqli_query($conn, $sql);
+  $sql2 = "INSERT INTO student(reg_no, username, passwd, email) VALUES('$regno', '$username', '$password', '$emailId');";
+  $result2 = mysqli_query($conn, $sql2);
 
-  if(! $result){
+  if(! $result2){
     echo "Couldn't insert into the table!";
   }
-  include "../landing.html";
+  include "../pages/signin.html";
+  // include "../pages/landing.html";
 ?>
