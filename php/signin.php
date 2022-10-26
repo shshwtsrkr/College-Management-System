5a4821d97708669a904d9d02f66f0ff854f4dfc2
Monirul <monirul.work.1729@gmail.com>
2022-10-27 01:41:21 +0530

    Semester registration HTML & PHP added

--- php/signin.php
+++ php/signin.php
@@ -1,26 +1,32 @@
 <?php
   include "connection.php";
+  
+  session_start();
 
-  $usrnm = $_POST["fname"];
-  $pswd = $_POST["passwd"];
-  $username = stripcslashes($usrnm);
-  $password = stripcslashes($pswd);
-  $username = mysqli_real_escape_string($conn,$username);
+  $regno = $_POST["regno"];
+  $passwd = $_POST["passwd"];
+  $regno = stripcslashes($regno);
+  $password = stripcslashes($passwd);
+  $regno = mysqli_real_escape_string($conn,$regno);
   $password = mysqli_real_escape_string($conn,$password);
+  $_SESSION['var']=$regno;
 
-  $sql = "SELECT * FROM student WHERE username='$username' and passwd='$password';";
+  $sql = "SELECT * FROM student WHERE reg_no='$regno' and passwd='$password';";
   $result = mysqli_query($conn, $sql);
 
   if (mysqli_num_rows($result) > 0) {
-    include "../pages/landing.html";
+    include "../pages/semesterRegistration.html";
     // output data of each row
     // echo "User";
     // while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-      //   echo "Username: " . $row["username"]. " - Password: " . $row["passwd"]. "<br>";
-      // }
+    //     echo "regno: " . $row["regno"]. " - Password: " . $row["passwd"]. "<br>";
+    //   }
   } else {
-    // echo "0 results";
+    echo "This is error from Sign in page";
     include "../pages/dummyError.html";
   }
+
+  // echo "This is error from Sign in page";
+  // include "../pages/dummyError.html";
 
 ?>
